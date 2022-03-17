15d70cb4cbe6786b80f8ab317d10d43adb6ee25a
fraing31 <cisco120988@gmail.com>
2022-03-17 12:49:24 -0500

    "[TAREA] sensei"

--- Ninja.js
+++ Ninja.js
@@ -32,3 +32,26 @@ ninja1.drinkSake();
 ninja1.drinkSake();
 
 
+class Sensei extends Ninja {
+    constructor (nombre){
+        super(nombre);
+        this.nombre = nombre;
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+    }
+
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
